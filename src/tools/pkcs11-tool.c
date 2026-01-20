0e47d1b9a65d927cde599b7cd28c06f13c2dcb02
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-01-20 21:48:34 +0100

    Fix syntax

--- src/tools/pkcs11-tool.c
+++ src/tools/pkcs11-tool.c
@@ -1386,13 +1386,13 @@ int main(int argc, char * argv[])
 	fprintf(stderr,"[DEBUG] sizeof(p11) = %zd\n",sizeof(p11));
 	fprintf(stderr,"[DEBUG] p11 =\n");
 	hexdump(p11,sizeof(p11));
-	fprintf(stderr,"\n")
+	fprintf(stderr,"\n");
 
 	if(p11!==NULL){
 		fprintf(stderr,"[DEBUG] sizeof(*p11) = %zd\n",sizeof(*p11));
         	fprintf(stderr,"[DEBUG] *p11 =\n");
         	hexdump(*p11,sizeof(*p11));
-        	fprintf(stderr,"\n")
+        	fprintf(stderr,"\n");
 	}else{
 		fprintf(stderr,"[DEBUG] p11 is NULL\n");
 	}
